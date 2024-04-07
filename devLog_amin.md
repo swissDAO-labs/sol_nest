f26ff337a59440ec7b736e0d54b879c1b6f98029
d0ra-th3-3xpl0ra <90798127+d0ra-1h3-3xpl0ra@users.noreply.github.com>
2024-04-07 15:48:18 +0200

    Added Boilerplate logic and modules

---
+++ devLog_amin.md
@@ -0,0 +1,7 @@
+## Thoughts and challenges
+
+Get evm nest.js NFT Minter 
+Tinker around with nest js calls and middleware
+
+And then go for solana 
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { SolanaModule } from './blockchain/solana/solana.module';
+import { GoogleCloudModule } from './cloud/google-cloud/google-cloud.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+    SolanaModule,
+    GoogleCloudModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
