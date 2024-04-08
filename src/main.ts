1bd3c6b333f3020170ae6f2d254aaa16cc6ba010
yenicelik <david.yenicelik@gmail.com>
2024-04-08 16:31:30 +0200

    trigger again

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addTag('Scarif')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(3000);
 }
